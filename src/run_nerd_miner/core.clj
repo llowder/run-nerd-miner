535ec4d4a90fe7d128ddf045c91a9c6d408f80b0
llowder <l.lowder@keepingyouhonest.net>
2014-11-21 00:19:41 -0600

    Broken Progress Can read the config file, and list the file in the datasource Reading the files in is not working.

--- src/run_nerd_miner/core.clj
+++ src/run_nerd_miner/core.clj
@@ -1,11 +1,41 @@
 (ns run-nerd-miner.core
   (:require [edn-config.core :refer [env]])
+  (:require [clojure.java.io :as io])
+  (:require [ clojure.data.xml :as xmld])
   (:gen-class))
+
+(defn only-files
+  [file-s]
+  (filter #(.isFile %) file-s)
+  )
+
+
+(defn read-xml-file
+  [s_file]
+    (let [input-xml (try
+                      (java.io.FileInputStream. s_file)
+                      (catch Exception e ))]
+      (if-not (nil? input-xml)
+        (xmld/parse input-xml)
+        nil))
+  )
+
+(defn read-in-files
+  (def directory (io/file (:source-data-dir env)))
+  (def files ( only-files (file-seq directory)))
+
+  (def xml-data {:ignore "this"})
+  
+  (doseq [f files]
+    (xml-data {(.getName f) (read-xml-file f)})
+    )
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "v: " (:config-version env) " g: " (:graph-data-dir env) " s: " (:source-data-dir env))
-  (pr-str env)
-  )
-
+  (def test-data (read-in-files))
+  (println test-data)
+  
+  
+)
